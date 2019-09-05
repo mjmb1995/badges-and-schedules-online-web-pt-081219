6275561dce66f57617cf957c0af790af35dea6ff
Mark Bihag <markbihag330@gmail.com>
2019-09-05 04:20:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  counter = 1
+  array.each do |name|
+    badge_maker(name)
+    assign_rooms(name, counter)
+    counter += 1
+  end
+end
+
+def assign_rooms(name, index)
+  puts "Hello, #{name}! You'll be assigned to room #{index}!"
+end
